611c1672a6d9ca2853eb4f57fa8781c37285c9fc
Ana Zuñiga <amzuniga24@itp.edu.co>
2026-09-19 14:24:17 -0500

    fix indentacion correcta en main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-def sumar(a, b): """Suma dos números.""" return a + b # prueba de branch protection 
+def sumar(a, b):
+    """Suma dos números."""
+    return a + b
+
+# prueba de branch protection
